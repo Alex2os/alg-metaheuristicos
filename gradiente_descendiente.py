56786cfeac9c891de7a9319bd7190f408c9dc854
zJaviOne <imalex1ne@gmail.com>
2026-08-26 15:39:08 -0600

    agregar gradiente descendiente

--- gradiente_descendiente.py
+++ gradiente_descendiente.py
@@ -5,42 +5,42 @@ import matplotlib.pyplot as plt
 # no es bueno que el learning rate sea tan grande ya que puede comportarse de forma muy greedy o agresiva, por lo que no le permitimos al algoritmo explorar.
 def Gradiente(x, y, Lr=0.01, iter = 1000):
     n = len(y)
-    x1 = 0.0 # se le pone 0.0 para especificar que el valor es un flotante.
-    x2 = 0.0
+    m = 0.0 # se le pone 0.0 para especificar que el valor es un flotante.
+    b = 0.0
 
     historial_costo = []
 
     for i in range(iter):
         # cambiar funcion objetivo
-        y_pred = x1*x**2 + x2*x + 2 # funcion cuadratica
-        costo = 1 / n * np.sum((y - y_pred)**2) # calculo error cuadratico medio
+        y_pred = m * x + b
+        costo = 1 / n * np.sum(x * (y - y_pred)**2) # calculo error cuadratico medio
         historial_costo.append(costo)
 
         # cambiar derivadas
-        x1_derivada = (-2/n) * np.sum(x*(y-y_pred)) # derivadas
-        x2_derivada = (-2/n) * np.sum((y-y_pred))
-        x1 = x1 - Lr * x1_derivada
-        x2 = x2 - Lr * x2_derivada
+        dm = (-2/n) * np.sum(x*(y-y_pred)) # derivadas
+        db = (-2/n) * np.sum(y-y_pred)
+        m = m - Lr * dm
+        b = b - Lr * db
 
-    return x1, x2, historial_costo
+    return m, b, historial_costo
 
 np.random.seed(42)
 
 X = np.array([1,2,3,4,5], dtype = float)
 Y = np.array([5,7,9,11,13], dtype = float)
 
-m_opt, b_opt, costo_hist = Gradiente(X, Y, Lr = 0.0002, iter=300)
+m_opt, b_opt, costo_hist = Gradiente(X, Y, Lr = 0.005, iter=500)
 
 # hay que escribir con acentos incluso si es en el código
-print(f"Valor óptimo (a): {m_opt:.4f}")
-print(f"Valor óptimo (b): {b_opt:.4f}")
+print(f"Pendiente óptima (m): {m_opt:.4f}")
+print(f"Intersección óptima (b): {b_opt:.4f}")
 print(f"Costo final: {costo_hist[-1]:.6f}")
 
 # agregar graficos
 plt.plot(costo_hist, marker = "o", markersize = 3)
-plt.xlabel("Iteraciones")
-plt.ylabel("Costo")
-plt.title("Gradiente")
+plt.xlabel("iteracion")
+plt.ylabel("mejor fitness encontrado")
+plt.title("Busqueda aleatoria en f(x) = 5cos(x)+cos(5x)")
 plt.grid(True)
 
 plt.show()
